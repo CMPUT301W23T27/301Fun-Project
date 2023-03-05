48800e8a2255daa5c863761bfcce35083da0114a
Yanmachine <hardingiant@gmail.com>
2023-03-05 14:29:14 -0700

    Xml for my ScannedCodePage. Changed theme colour to #32b768 Color Hex

---
+++ app/src/main/java/com/example/qrmon/ScannedCodePage.java
@@ -0,0 +1,14 @@
+package com.example.qrmon;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ScannedCodePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scanned_code_page);
+    }
+}
