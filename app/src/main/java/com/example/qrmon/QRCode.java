89f181f82482c6b3e51297928af38535bbf8043c
Joel Weller <97563644+jweller55@users.noreply.github.com>
2023-03-07 01:25:39 -0700

    AddInterpreter

--- app/src/main/java/com/example/qrmon/QRCode.java
+++ app/src/main/java/com/example/qrmon/QRCode.java
@@ -8,4 +8,38 @@ public class QRCode {
     private ImageView picture;
     private String comment;
     private String geolocation;
+    private String hash;
+    private String url;
+    private Integer score;
+
+    QRCode(String name, ImageView visual, ImageView picture, String comment, String geolocation, String hash, String url, Integer score) {
+        this.name = name;
+        //this.visual = visual;
+        //this.picture = picture;
+        this.comment = comment;
+        this.geolocation = geolocation;
+        this.url = url;
+        this.score = score;
+    }
+
+    public String getName() {return name;}
+    public ImageView getVisual() {return visual;}
+    public ImageView getPicture() {return picture;}
+    public String getComment() {return comment;}
+    public String getGeolocation() {return geolocation;}
+    public String getHash() {return hash;}
+    public String getUrl() {return url;}
+    public Integer getScore() {return score;}
+
+
+
+    public void setName(String newName) {this.name= newName;}
+    public void setVisual(ImageView newVisual) {this.visual = newVisual;}
+    public void setPicture(ImageView newPicture) {this.picture = newPicture;}
+    public void setComment(String newComment) {this.comment = newComment;}
+    public void setGeolocation(String newGeolocation) {this.geolocation = newGeolocation;}
+    public void setHash(String newHash) {this.hash = newHash;}
+    public void setUrl(String newUrl) {this.url = newUrl;}
+    public void setScore(Integer newScore) {this.score = newScore;}
+
 }
