559ea2d7f248925f7c1be67b1dc4533cb37e4d89
Joel Weller <97563644+jweller55@users.noreply.github.com>
2023-03-21 21:32:24 -0600

    basic_maps_framework_added

--- app/src/main/java/com/example/qrmon/MapsFragment.java
+++ app/src/main/java/com/example/qrmon/MapsFragment.java
@@ -8,12 +8,21 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.Objects;
+
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link MapsFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class MapsFragment extends Fragment {
+public class MapsFragment extends Fragment implements OnMapReadyCallback {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -55,10 +64,45 @@ public class MapsFragment extends Fragment {
         }
     }
 
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_maps, container, false);
+        View view = inflater.inflate(R.layout.fragment_maps, container, false);
+        // Get the SupportMapFragment and request notification when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+        return view;
+    }
+    // [END maps_marker_get_map_async]
+    // [END_EXCLUDE]
+
+    // [START_EXCLUDE silent]
+    /**
+     * Manipulates the map when it's available.
+     * The API invokes this callback when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user receives a prompt to install
+     * Play services inside the SupportMapFragment. The API invokes this method after the user has
+     * installed Google Play services and returned to the app.
+     */
+    // [END_EXCLUDE]
+    // [START maps_marker_on_map_ready_add_marker]
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        // [START_EXCLUDE silent]
+        // Add a marker
+        // and move the map's camera to the same location.
+        // [END_EXCLUDE]
+        LatLng coordinates = new LatLng(53.523496, -113.518332);
+        googleMap.addMarker(new MarkerOptions()
+                .position(coordinates)
+                .title("Ildar's live masterclass"));
+        // [START_EXCLUDE silent]
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(coordinates));
+        // [END_EXCLUDE]
     }
 }
